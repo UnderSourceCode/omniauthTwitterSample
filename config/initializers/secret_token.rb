592b195087ee90bef1e72f63d8c86ed7765fbe9a
UnderSourceCode <UnderSourceCode@gmail.com>
2013-04-14 15:33:31 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthTwitterSample::Application.config.secret_token = '563721b7013d431da912d520c7e9c57f67105acd90a388110cdd0955606ed88f6c665753ab46c4a626d316bc5cc2691a8df93a50f9df4b02a38ca2ec66e1f7fe'
